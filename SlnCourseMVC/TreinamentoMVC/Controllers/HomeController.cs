50046855132cb082736aea1a1533a4deebe50405
Migbari <webdevel.bin@gmail.com>
2020-11-01 01:26:43 -0300

    teste 2

---
+++ SlnCourseMVC/TreinamentoMVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using TreinamentoMVC.Models;
+
+namespace TreinamentoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Miguel Batista Ribeiro",
+                Tipo = "Administrador"
+            };
+
+            // Retorno por ViewData // 
+            //ViewData["PessoaId"] = pessoa.PessoaId;
+            //ViewData["Nome"] = pessoa.Nome;
+            //ViewData["Tipo"] = pessoa.Tipo;
+
+            // Retorno por ViewBag // 
+            //ViewBag.Id = pessoa.PessoaId;
+            //ViewBag.nome = pessoa.Nome;
+            //ViewBag.tipo = pessoa.Tipo;
+
+            return View(pessoa);
+        }
+        public ActionResult Contato()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)
+        {
+            //ViewData["PessoaId"] = pessoa.PessoaId;
+            //ViewData["Nome"] = pessoa.Nome;
+            //ViewData["Tipo"] = pessoa.Tipo;
+
+            return View(pessoa);
+        }
+    }
+}
---
+++ SlnCourseMVC/TreinamentoMVC/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TreinamentoMVC.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        public string Nome { get; set; }
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "De 5 a 50 caracteres")]
+        public string Observacoes { get; set; }
+        [Range(18, 70, ErrorMessage = "Idade entre 18 e 70 anos apenas")]
+        public int Idade { get; set; }
+        [Display(Name = "Email ou Telefone")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$|^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}", ErrorMessage = "Digite um email ou número válido")]
+        public string Email { get; set; }
+        [Display(Name = "Nome de Login")]
+        [RegularExpression(@"[a-zA-Z]{5,15}", ErrorMessage = "Deve conter letras de 5 a 15 caracteres")]
+        [Required(ErrorMessage = "O login é obrigatório")]
+        [Remote("LoginUnico", "Usuario", ErrorMessage = "Este login já existe")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "A Senha é obrigatória")]
+        public string Senha { get; set; }
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "Senhas não são iguais")]
+        public string ConfirmaSenha { get; set; }
+
+    }
+}
